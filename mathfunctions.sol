1dbd60477d29acce38b46c1fc673aa5c2a2eb18b
burhox <hoxhabur@gmail.com>
2018-04-21 11:13:58 +0200

    NdryshimetBH

--- mathfunctions.sol
+++ mathfunctions.sol
@@ -6,10 +6,18 @@ contract mathoperations
     int a = 3; 
     int b = 4;
     int  value;
-    
+    function substraction() public
+     {
+      value =a /b
+      }
+function getvalue () public view returns (int)
+    {
+     return value
+    }
     function addition() public 
     {
-        value =a +b;
+       value =a +b;
+      
     }
     function getvalue () public view returns (int)
     {
