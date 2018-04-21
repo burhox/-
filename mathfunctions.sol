97875f996da98bf18ce7c7f00462b382745cddb9
shegalikaj <shegalikaj@gmail.com>
2018-04-21 10:51:35 +0200

    first function: addition

---
+++ mathfunctions.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.0;
+
+
+contract mathoperations 
+{
+    int a = 3; 
+    int b = 4;
+    int  value;
+    
+    function addition() public 
+    {
+        value =a +b;
+    }
+    function getvalue () public view returns (int)
+    {
+        return value;
+    }
+}
